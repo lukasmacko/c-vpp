e9683c888335e4054c3457752fdaa53dfb4da919
Rastislav Szabo <raszabo@cisco.com>
2017-09-27 09:45:00 +0200

    added go:generate rule for CNI proto

--- plugins/contiv/plugin_impl_contiv.go
+++ plugins/contiv/plugin_impl_contiv.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate protoc -I ./model/cni --go_out=plugins=grpc:./model/cni ./model/cni/cni.proto
+
 package contiv
 
 import (
