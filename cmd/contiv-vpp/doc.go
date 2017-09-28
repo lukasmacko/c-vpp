02f59bdd6ba15fb1191fe75912c7340cd9713213
Rastislav Szabo <raszabo@cisco.com>
2017-09-28 15:27:28 +0200

    doc.go for CNI

--- cmd/contiv-vpp/doc.go
+++ cmd/contiv-vpp/doc.go
@@ -1,3 +1,3 @@
-// Contiv-vpp is an agent that accepts configuration from CNI-plugin
-// and applies it on the vpp in vswitch.
+// Contiv-vpp is a CNI plugin that forwards the CNI requests to the gRPC server
+// specified in the CNI config file.
 package main
